e04174d0bb4dcb7ef342f7bbbe54ce643999a566
jlin96 <Jesse13234@gmail.com>
2020-06-24 17:29:02 -0400

    can select multiple countrys/ grouped barchart shown

--- src/index.js
+++ src/index.js
@@ -1,23 +1,162 @@
 import './styles/index.scss';
 import utils from "./lib/utils"
 
+const DIMENSIONS = {
+    width: 1000,
+    height: 500
+}
+
 document.addEventListener("DOMContentLoaded", () => {
     utils.fetchCountries();
-    let selectedValue = document.getElementsByClassName('countries-selector');
-    let comparedValue = document.getElementsByClassName('compared-countries');
-    selectedValue = selectedValue[0];
-    comparedValue = comparedValue[0];
+    utils.fetchCountries2();
+
+    //gets country and compare selector to setup change event listeners
+    const selectedValue = document.querySelector('select.countries-selector');
+    const comparedValue = document.querySelector('select.compared-countries');
+
+    //gets checkout to check for changes
+    const checkbox = document.querySelector("input.state-checkbox");
 
     selectedValue.addEventListener("change", e => {
         for (let i = 0; i < comparedValue.length; i++) {
-            if(comparedValue[i].value === e.target.value) {
+            if (e.target.value === comparedValue[i].value) {
                 utils.getDisabledElement();
-                utils.setNewDisabledElement(comparedValue[i].value)
+                utils.setNewDisabledElement(comparedValue[i].value);
+
+                let usa = document.querySelector("div.states-wrapper");
+                if (e.target.value === "USA") {
+                  if (comparedValue[i].value === "USA") {
+                    usa.classList.remove("hidden");
+                  }
+                } else {
+                  usa.classList.add("hidden");
+                }
             }
         }
+        clearGraph();
+        drawBarGraph(countries);
     })
   
     comparedValue.addEventListener("change", e => {
+        clearGraph();
+        drawBarGraph(countries);
     })
-    utils.getAllStatistics();
+
+    checkbox.addEventListener('change', e => {
+        if(checkbox.checked) {
+            e.target.value = 'state';
+        } else {
+            e.target.value = 'nstate';
+        }
+    })
+
+    let checkboxShown = false;
+    
+    const selectcheckbox = document.querySelector("div.select-box-wrapper");
+
+    selectcheckbox.addEventListener('click', e => {
+        const checkbox = document.querySelector("div.checkboxes");
+        if (!checkboxShown) {
+            checkbox.classList.remove("hidden");
+            checkboxShown = true;
+        } else {
+            checkbox.classList.add("hidden");
+            checkboxShown = false;
+        }
+    })
+    
+    const countries = utils.getAllStatistics();
+    drawBarGraph(countries);
+    const states = utils.getAllUSStatistics();
 })
+
+function clearGraph() {
+    d3.select("svg").remove();
+}
+
+
+function drawBarGraph(data, country, compared) {
+    let margin = { top: 30, right: 20, bottom: 30, left: 60 }
+
+    data.then(result => {
+        let countrySelect = document.querySelector("select.countries-selector");
+        country = country || countrySelect.options[countrySelect.selectedIndex];
+        let compareSelect = document.querySelector("select.compared-countries");
+        compared = compared || compareSelect.options[compareSelect.selectedIndex];
+
+        const xAxisData = [];
+        const yAxisData = [];
+        // console.log(result);
+        let both = [];
+
+        result.forEach(ele => {
+            if(ele.country === country.value || ele.country === compared.value) {
+                xAxisData.push(ele.country);
+                yAxisData.push(ele.cases);
+                both.push(ele);
+            }
+        })
+
+        let subgroups = Object.keys(both[0]).slice(1);
+        console.log(subgroups);
+
+        let groups = d3.map(both, function(d){return(d.country)}).keys()
+        console.log(groups);
+
+        let svg = d3
+          .select("#svgcontainer")
+          .append("svg")
+          .attr("width", DIMENSIONS.width + margin.left + margin.right)
+          .attr("height", DIMENSIONS.height + margin.top + margin.bottom)
+          .append("g")
+          .attr( "transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        // X scale and Axis
+        let x = d3.scaleBand()
+            .domain(groups)
+            .range([0, DIMENSIONS.width])
+            .padding([0.2])
+
+        svg.append("g")
+            .attr("transform", "translate(0," + DIMENSIONS.height + ")")
+            .call(d3.axisBottom(x).tickSize(0));
+
+        //y axis
+
+        const maxHeight = (d3.max(yAxisData) / 9) * 10;
+        let y = d3.scaleLinear()
+            .domain([0, maxHeight])
+            .range([ DIMENSIONS.height, 0 ]);
+
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+        let xSubgroup = d3
+            .scaleBand()
+            .domain(subgroups)
+            .range([0, x.bandwidth()])
+            .padding([0.05]);
+
+        let color = d3
+            .scaleOrdinal()
+            .domain(subgroups)
+            .range(["#a6cee3", "#1f78b4", "#b2df8a", "#33a02c", "#fb9a99", "#fdbf6f", "#cab2d6", "#6a3d9a", "#ffff99", "#b15928"]);
+
+            console.log(both)
+        svg.append("g")
+            .selectAll("g")
+            // Enter in data = loop group per group
+            .data(both)
+            .enter()
+            .append("g")
+            .attr("transform", function(d) { return "translate(" + x(d.country) + ",0)"; })
+            .selectAll("rect")
+            .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
+            .enter().append("rect")
+            .attr("x", function(d) { return xSubgroup(d.key); })
+            .attr("y", function(d) { return y(d.value); })
+            .attr("width", xSubgroup.bandwidth())
+            .attr("height", function(d) { return DIMENSIONS.height - y(d.value); })
+            .attr("fill", function(d) { return color(d.key); });
+    })
+}
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,5 +1,21 @@
-import axios from 'axios';
 const Util = {
+    dynamicSort(property) {
+        var sortOrder = 1;
+
+        if(property[0] === "-") {
+            sortOrder = -1;
+            property = property.substr(1);
+        }
+
+        return function (a,b) {
+            if(sortOrder == -1){
+                return b[property].localeCompare(a[property]);
+            }else{
+                return a[property].localeCompare(b[property]);
+            }        
+        }
+    },
+    //sets up all countrys for select html
     fetchCountries() {
         let countries;
         fetch("https://coronavirus-19-api.herokuapp.com/countries")
@@ -8,9 +24,10 @@ const Util = {
             .then(() => {
                 const countrySelector = document.getElementsByClassName('countries-selector');
                 const comparedAgainst = document.getElementsByClassName('compared-countries');
-
                 
+                countries.sort(Util.dynamicSort('country'));
 
+                console.log(countries);
                 for (let i = 0; i < countries.length; i++) {
                     let selected = document.createElement('option');
                     selected.value = countries[i].country;
@@ -22,19 +39,50 @@ const Util = {
                     }
                 }
 
+                let selected = document.createElement("option");
+                selected.value = 'None';
+                selected.innerHTML = 'None';
+                selected.classList.add("compared-countries-options");
+                comparedAgainst[0].appendChild(selected);
+                selected.disabled = true;
+                selected.selected = true;
+
                 for (let i = 0; i < countries.length; i++) {
                     let selected = document.createElement('option');
                     selected.value = countries[i].country;
                     selected.innerHTML = countries[i].country;
                     selected.classList.add('compared-countries-options')
                     comparedAgainst[0].appendChild(selected);
-                    if(i === 0 ) {
-                        selected.disabled = true;
-                        selected.selected = true;
-                    }
                 }
             })
     },
+    fetchCountries2() {
+        let countries;
+        fetch("https://coronavirus-19-api.herokuapp.com/countries")
+            .then(response => response.json())
+            .then(result => countries = result)
+            .then(() => {
+                const countrySelector = document.querySelector('div.checkboxes');
+                
+                countries.sort(Util.dynamicSort('country'));
+                
+                console.log(countries);
+                for (let i = 0; i < countries.length; i++) {
+                    let outerDiv = document.createElement('div');
+                    outerDiv.classList.add("checkbox-wrapper");
+                    let input = document.createElement('input');
+                    input.type = "checkbox";
+                    input.classList.add("checkbox-box");
+                    input.value = countries[i].country;
+                    outerDiv.appendChild(input);
+                    let label = document.createElement('label');
+                    label.innerHTML = countries[i].country;
+                    outerDiv.appendChild(label);
+                    countrySelector.appendChild(outerDiv);
+                }
+            })
+    },
+    //finds disabled element and sets it false
     getDisabledElement() {
         let comparedAgainst = document.getElementsByClassName('compared-countries');
         comparedAgainst = comparedAgainst[0];
@@ -45,6 +93,7 @@ const Util = {
             }
         }
     },
+    //sets new disabled element to true
     setNewDisabledElement(disabledValue) {
         let comparedAgainst = document.getElementsByClassName('compared-countries');
         comparedAgainst = comparedAgainst[0];
@@ -54,11 +103,15 @@ const Util = {
             }
         }
     },
+    //gets all countries statistics
     getAllStatistics() {
-        // axios.get("/allstatistics").then(console.log("success"));
-        fetch("https://covidtracking.com/api/v1/us/current.json")
-          .then((response) => response.json())
-          .then((result) => console.log(result));
+        return fetch("https://coronavirus-19-api.herokuapp.com/countries")
+          .then( response => response.json())
+    },
+    //gets all states statistics
+    getAllUSStatistics() {
+        return fetch("https://covidtracking.com/api/v1/states/daily.json")
+            .then(response => response.json())
     }
 }
 
